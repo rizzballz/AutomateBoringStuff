2679d3cd2826db717ccbbe3bcc00436b458303c3
rizzballz <Ryan.j.mcmillan34@gmail.com>
2019-12-02 20:46:00 +1100

    Update guess.py

--- 5. Creating a program from scratch/guess.py
+++ 5. Creating a program from scratch/guess.py
@@ -1,1 +1,30 @@
+# Guess the number game from Automate the Boring Stuff from memory
+# By Ryan McMillan Dec 2019
+
+import random
+
+TITLE = 'GUESSING GAME'
+print(TITLE.center(10,' '))
+print(len(TITLE) * '-')
+
+print('What is your name?')
+name = str(input())
+
+secret_number = random.randint(1, 20)
+print('What is your first Guess?')
+for guess_number in range(1, 6):
+    guess = int(input())
+
+    if guess < secret_number:
+        print('Your guess is too low..')
+    elif guess > secret_number:
+        print('Your guess is too high..')
+    else:
+        # This is if the contestant guesses right. break the loop and continue on
+        break
+
+if guess == secret_number:
+    print('You win ' + name + '! It took you ' + str(guess_number) + ' guesses')
+else:
+    print('You Lose! Better luck next time.')
 
